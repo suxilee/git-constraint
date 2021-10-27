c740aebe2691fa68d933df97efcbe062e98aea9e
linx <130480213@qq.com>
2021-10-27 10:39:08 +0800

    feat(Demo01): New feature Demo01
    Demo01功能实现

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # git & idea 使用笔记
+
+## 1.相关文章
+
+[IDEA intellij & Git 学习笔记01  创建项目&&推送至远程仓库](https://www.ovolll.cn/archives/idea-git01)
+
---
+++ src/main/java/com/lansu/git/Demo01.java
@@ -0,0 +1,13 @@
+package com.lansu.git;
+
+/**
+ * @version v1.0
+ * @Author linx
+ * @CreateTime 2021-10-27 9:45
+ * @Description
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("demo01");
+    }
+}
